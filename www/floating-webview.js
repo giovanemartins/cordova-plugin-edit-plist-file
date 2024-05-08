169073a987a7d861c092821e3db704a4a16cbcdf
Giovane Martins <3141734+giovanemartins@users.noreply.github.com>
2024-05-08 01:15:04 -0300

    Remove Floating Webview - restore operational version

--- www/floating-webview.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.show = function(arg0, success, error) {
-  exec(success, error, "FloatingWebview", "show", [arg0]);
-};
-
-exports.hide = function(arg0, success, error) {
-  exec(success, error, "FloatingWebview", "hide", [arg0]);
-};
